adedb78b78afe2c12584b96f2dcbce6b6f3a55a1
samiksha <samiksha.dev98@gmail.com>
2023-05-26 14:38:47 +0530

    Movies API Backend Code Commit

---
+++ src/main/java/dev/samiksha/movies/ReviewService.java
@@ -0,0 +1,27 @@
+package dev.samiksha.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.CriteriaExtensionsKt;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("ImdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
---
+++ src/test/java/dev/samiksha/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.samiksha.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
